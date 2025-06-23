f247dd08c9bca6acb8437b5862805a9490baf129
Shadowmarker789 <qwertydavid@gmail.com>
2025-06-23 23:54:24 +0800

    Doing some thinking of how to proceed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // Simple JavaScript to demonstrate interactivity
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Hello World app loaded!');
+    document.getElementById("loadingContent").style.display = "none";
+    document.getElementById("finishedLoadingContent").style.display = "inline";
 });
